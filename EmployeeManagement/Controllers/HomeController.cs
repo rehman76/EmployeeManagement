b3dea57fc73b6d21934b8c572e491e5d342e9622
rehman76 <rehman.ali76@outlook.com>
2019-06-12 23:39:06 +0500

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index() {
+            return Json(new {id=1,name="Rehman Ali"});;
+        }
+    }
+}
